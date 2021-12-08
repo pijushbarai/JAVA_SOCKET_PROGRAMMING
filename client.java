de103b802e2a64d0b981f56a64c3b6c54bedab7e
pijushbarai <pijushbarai1@gmail.com>
2021-12-09 02:28:07 +0600

    a simple chat box is created between server and client by java socket programming

--- client.java
+++ client.java
@@ -5,29 +5,31 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class client{
-    public static void main(String[] args) throws  IOException {
+public class client {
+    public static void main(String[] args) throws IOException {
+        String message;
+        System.out.println("Client Started");
+        do {
+            Socket socket = new Socket("127.0.0.1", 22222);
+            //System.out.println("client started");
+
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            Scanner sc = new Scanner(System.in);
+            message = sc.nextLine();
+
+            oos.writeObject(message);
+            try {
+                Object smsg = ois.readObject();
+                System.out.println("Server : " + (String) smsg);
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println(e);
+            }
+        }while(!message.equals("bye"));
         
-        Socket socket = new Socket("127.0.0.1",22222);
-        System.out.println("client started");
-        
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
-
-        Scanner sc = new Scanner(System.in);
-        String message = sc.nextLine();
-
-        oos.writeObject(message);
-        try {
-            Object smsg = ois.readObject();
-            System.out.println("Message return from server : "+(String)smsg);
-            
-        } catch (Exception e) {
-            //TODO: handle exception
-            System.out.println(e);
-        }
-        
     }
 }
--- server.java
+++ server.java
@@ -3,15 +3,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class server{
     public static void main(String[] args) throws IOException {
         ServerSocket serversocket = new ServerSocket(22222);
         System.out.println("Server started");
-
+        String chk;
         while(true){
             Socket socket = serversocket.accept();
-            System.out.println("Server accept socket");
+            //System.out.println("Server accept socket");
 
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
@@ -19,12 +20,16 @@ public class server{
             try {
                 //input read from client
                 Object cmsg = ois.readObject();
-                System.out.println("msg from client to server : "+ (String)cmsg);
-
+                System.out.println("Client : "+ (String)cmsg);
+                //chk = (String)cmsg;
                 //replay to client
-                String serverReply = (String)cmsg;
-                serverReply = serverReply.toUpperCase();
-                oos.writeObject(serverReply);
+
+                // String serverReply = (String)cmsg;
+                // serverReply = serverReply.toUpperCase();
+                // oos.writeObject(serverReply);
+                Scanner sc = new Scanner(System.in);
+                chk = sc.nextLine();
+                oos.writeObject(chk);
 
             } catch (ClassNotFoundException e) {
                 // TODO Auto-generated catch block
